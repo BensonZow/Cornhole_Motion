3193ab6f57e52fac0106a696b7044e07064c4a39
cornholio <cornholio.com>
2026-03-11 16:22:52 -0600

    tester serial script

---
+++ src/arduino_serial_com/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'arduino_serial_com'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cornholio',
+    maintainer_email='cornholio.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'sender_node = arduino_serial_com.serial_sender:main',
+        ],
+    },
+)
