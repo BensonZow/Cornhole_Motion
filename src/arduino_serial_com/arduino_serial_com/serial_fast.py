27efbb7be7420d788794cbc3054aba3bed158cac
cornholio <cornholio.com>
2026-04-06 17:17:24 -0600

    updated serial code to have end lines

--- src/arduino_serial_com/arduino_serial_com/serial_fast.py
+++ src/arduino_serial_com/arduino_serial_com/serial_fast.py
@@ -4,9 +4,9 @@ from rclpy.node import Node
 from std_msgs.msg import String
 import serial
 
-serial_port = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+serial_port = serial.Serial('/dev/ttyACM1', 115200, timeout=1)
 
 while(True):
     msg=input()
-    serial_port.write(msg.encode('utf-8'))
+    serial_port.write((msg+ '\n').encode('utf-8'))
     print(f'Sent to Arduino: "{msg}"')
